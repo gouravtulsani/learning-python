dbb37a6dee97a7550b6f3e5cc632fe3620470016
gouravtulsani <tulsanig1432@gmail.com>
2016-11-05 12:29:50 +0530

    scrapping NYT

--- webscrap1.py
+++ webscrap1.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 
 url = "http://www.nytimes.com/"
 r = requests.get(url)
+soup = BeautifulSoup(r.text)
+#print(soup)
 
-soup = BeautifulSoup(r.content)
+g_data = soup.find_all(class_="story-heading")
 
-
-g_data = soup.find_all("article", {"class": "story"})
 for item in g_data:
-	print(item.find_all("h2",{"class":"story-heading"})[0].text)
+	print(item.text.replace("\n" , " ").strip())
